d1df4b07a9e50c14c3fad2a00284042fad9a90c7
派蒙 <75768445+paimengpaifjb@users.noreply.github.com>
2024-09-14 21:48:33 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,8 +1,30 @@
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
-    url.host = "api.openai-proxy.org";
-    // openai is already set all CORS heasders
+    if (url.pathname.endsWith('.html') || url.pathname.endsWith('.htm') || url.pathname.endsWith('/')) {
+      return new Response("404 Not Found", { status: 404 });
+    }
+
+    if (request.method === 'POST' && request.headers.get('Content-Type') === 'application/json') {
+      const body = await request.json();
+
+      // Check if the request is for GPT-4 or GPT-4-turbo
+      if (body.model === 'gpt-4' || body.model === 'gpt-4-turbo-preview' || body.model === 'gpt-4-vision-preview' || body.model === 'gpt-4-0125-preview' || body.model === 'gpt-4o' || body.model === 'gpt-4-turbo') {
+        body.model = 'gpt-4o-mini'; 
+      }
+
+      const modifiedRequest = new Request(request.url, {
+        headers: request.headers,
+        method: request.method,
+        body: JSON.stringify(body),
+        redirect: 'follow'
+      });
+
+      url.host = "api.openai-proxy.org"; 
+      return fetch(url, modifiedRequest);
+    }
+
+    url.host = "api.openai-proxy.org"; 
     return fetch(url, {
       headers: request.headers,
       method: request.method,
